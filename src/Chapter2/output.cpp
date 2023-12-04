d86877243ec2b45f874e46ae741ee100c453ff46
Arthur Jeulin <arthur.jeulin@free.fr>
2023-12-04 18:26:03 +0100

    Fix output.cpp const_buffer

--- src/Chapter2/output.cpp
+++ src/Chapter2/output.cpp
@@ -8,8 +8,8 @@ int main()
     buf ="Hello"; // Step 1 and 2 in single line.
     //Step 3. Creating buffer representation that satifies
     // ConstBufferSequence conept requirements.
-    boost::asio::const_buffers_1 output_buf = boost::asio::buffer(buf);
-
+    boost::asio::const_buffer output_buf(
+        boost::asio::buffer(buf));
     //Step 4. 'output_buf' is the representation of the
     // buffer 'buf' that can be used in Boost.Asioi output
     // operations
