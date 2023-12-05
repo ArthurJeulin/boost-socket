c28cfbd300d56754b46d6e67524ec57dd6efc2bd
Arthur Jeulin <arthur.jeulin@free.fr>
2023-12-05 15:44:19 +0100

    Added stream.cpp

---
+++ src/Chapter2/stream.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <boost/asio.hpp>
+
+int main()
+{
+    boost::asio::streambuf buf;
+    std::ostream output(&buf);
+
+    //Writing the message to the stream-based buffer.
+    output << "Message1\nMessage2";
+    
+    //Now we want to read all data from a streambuf
+    //until '\n' delilmiter.
+    //Instantiate an nput stream wichi uses our
+    //stream buffer.
+    std::istream input(&buf);
+
+    //We'll read data into this string.
+    std::string message1;
+    std::getline(input,message1);
+
+    //Now message1 string contains 'Message1'.
+
+    return 0;
+}
