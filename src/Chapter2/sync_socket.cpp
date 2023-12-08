af1140622a2dd6cbbb64caf3365f5ac95805020d
Arthur Jeulin <arthur.jeulin@free.fr>
2023-12-08 09:54:04 +0100

    Update Sync_socket

--- src/Chapter2/sync_socket.cpp
+++ src/Chapter2/sync_socket.cpp
@@ -29,11 +29,7 @@ int main()
     }
     catch(const boost::system::system_error& e)
     {
-<<<<<<< HEAD
         std::cout << "Error-occured !"
-=======
-        std::cout << "Il y a un problème ici"
->>>>>>> test-conflict
                   << "Error code = "<< e.code()
                   <<". Message: " << e.what();
         return e.code().value();
